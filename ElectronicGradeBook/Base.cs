b8c5679c6b90fe2422a65b86fb0bc693d5a06f0d
denzelcrocker <zelentsov36@gmail.com>
2022-05-08 19:07:47 +0300

    final

---
+++ ElectronicGradeBook/Base.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Electronic
+{
+    internal class Base
+    {
+        public static void ReadStudents()
+        {
+            StreamReader students = new StreamReader(@"C:\Users\zelen\Desktop\ElectronicGradeBook\ElectronicGradeBook\\students.txt");
+
+            string line = students.ReadLine();
+            string[] start = { "ФИО студента", "Год зачисления", "Форма обучения", "Специальность" };
+            string table = start[0];
+            int i = 0;
+
+            Console.WriteLine($"|{$"{"--"}",2}-|-{$"{"------------------------------------"}",35}----|-{$"----------------",15}----|-{$"----------------",15}----|-{$"---------------",15}-|");
+            Console.WriteLine($"|{$"{"№"}",2} | {$"{start[0]}",35}     | {$"{start[1]}",15}     | {$"{start[2]}",15}     | {$"{start[3]}",15} |");
+            Console.WriteLine($"|{$"{"--"}",2}-|-{$"{"------------------------------------"}",35}----|-{$"----------------",15}----|-{$"----------------",15}----|-{$"---------------",15}-|");
+           
+            while (line != null)
+            {
+                i++;
+                string[] splitLine = line.Split(";");
+                string fio = splitLine[0];
+
+                Console.WriteLine($"|{$"{i}", 2} | {$"{splitLine[0]}", 35}     | {$"{splitLine[1]}",15}     | {$"{splitLine[2]}",15}     | {$"{splitLine[3]}",15} |" );
+                Console.WriteLine($"|{$"{"--"}",2}-|-{$"{"------------------------------------"}",35}----|-{$"----------------",15}----|-{$"----------------",15}----|-{$"---------------",15}-|");
+
+                line = students.ReadLine();
+            }
+            students.Close();
+        }
+
+        public static void PlusStudents(string[] DataPlus)
+        {
+            StreamWriter students = new StreamWriter(@"C:\Users\zelen\Desktop\ElectronicGradeBook\ElectronicGradeBook\\students.txt", append: true);
+
+            string line = $"{DataPlus[0]};{DataPlus[1]};{DataPlus[2]};{DataPlus[3]}";
+
+            students.WriteLine(line);
+            students.Close();
+        }
+
+        public static void SearchStudents()
+        {
+            Console.Clear();
+            StreamReader students = new StreamReader(@"C:\Users\zelen\Desktop\ElectronicGradeBook\ElectronicGradeBook\\students.txt");
+
+            string line = students.ReadLine();
+            int i = 0;
+            int a = 0;
+
+            Console.WriteLine("Выберите столбец, по которому вы хотите совершить поиск по справочнику: \n \n 0 - ФИО студента \n 1 - Год зачисления \n 2 - Форма обучения \n 3 - Специальность \n");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Введите слово для поиска по справочнику: \n");
+            string word = Console.ReadLine();
+            Console.WriteLine();
+            
+            Console.Clear();
+            Console.WriteLine($"Результаты поиска: \n");
+            string[] start = { "ФИО студента", "Год зачисления", "Форма обучения", "Специальность" };
+            string table = start[0];
+
+            Console.WriteLine($"|{$"{"--"}",2}-|-{$"{"------------------------------------"}",35}----|-{$"----------------",15}----|-{$"----------------",15}----|-{$"---------------",15}-|");
+            Console.WriteLine($"|{$"{"№"}",2} | {$"{start[0]}",35}     | {$"{start[1]}",15}     | {$"{start[2]}",15}     | {$"{start[3]}",15} |");
+            Console.WriteLine($"|{$"{"--"}",2}-|-{$"{"------------------------------------"}",35}----|-{$"----------------",15}----|-{$"----------------",15}----|-{$"---------------",15}-|");
+
+            while (line == word || line != null)
+            {
+                a++;
+                string[] splitLine = line.Split(";");
+                string search = splitLine[b];
+                line = students.ReadLine();
+
+                if (word.Contains(search))
+                {
+                    Console.WriteLine($"|{$"{a}",2} | {$"{splitLine[0]}",35}     | {$"{splitLine[1]}",15}     | {$"{splitLine[2]}",15}     | {$"{splitLine[3]}",15} |");
+                    Console.WriteLine($"|{$"{"--"}",2}-|-{$"{"------------------------------------"}",35}----|-{$"----------------",15}----|-{$"----------------",15}----|-{$"---------------",15}-|");
+                    i = 1;
+                }
+                else
+                {
+
+                }
+            }
+            if (i == 1)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine(" \n Записи с таким содержимым нет \n");
+            }
+            students.Close();
+        }
+        public static void ReadGrade()
+        {
+            StreamReader grades = new StreamReader(@"C:\Users\zelen\Desktop\ElectronicGradeBook\ElectronicGradeBook\\grades.txt");
+
+            string line = grades.ReadLine();
+            string[] start = { "ФИО студента", "Предмет", "Оценка" };
+            string table = start[0];
+            int i = 0;
+
+            Console.WriteLine($"|{$"{"--"}",2}-|-{$"{"------------------------------------"}",35}----|-{$"----------------",15}----|-{$"---------------",15}-|");
+            Console.WriteLine($"|{$"{"№"}",2} | {$"{start[0]}",35}     | {$"{start[1]}",15}     | {$"{start[2]}",15} |");
+            Console.WriteLine($"|{$"{"--"}",2}-|-{$"{"------------------------------------"}",35}----|-{$"----------------",15}----|-{$"---------------",15}-|");
+
+            while (line != null)
+            {
+                i++;
+                string[] splitLine = line.Split(";");
+                string fio = splitLine[0];
+
+                Console.WriteLine($"|{$"{i}",2} | {$"{splitLine[0]}",35}     | {$"{splitLine[1]}",15}     | {$"{splitLine[2]}",15} |");
+                Console.WriteLine($"|{$"{"--"}",2}-|-{$"{"------------------------------------"}",35}----|-{$"----------------",15}----|-{$"---------------",15}-|");
+                line = grades.ReadLine();
+            }
+            grades.Close();
+        }
+        public static void PlusGrade(string[] DataPlus)
+        {
+            StreamWriter grades = new StreamWriter(@"C:\Users\zelen\Desktop\ElectronicGradeBook\ElectronicGradeBook\\grades.txt", append: true);
+
+            string line = $"{DataPlus[0]};{DataPlus[1]};{DataPlus[2]}";
+            grades.WriteLine(line);
+            grades.Close();
+        }
+        public static void SearchGrades()
+        {
+            Console.Clear();
+            StreamReader grades = new StreamReader(@"C:\Users\zelen\Desktop\ElectronicGradeBook\ElectronicGradeBook\\grades.txt");
+
+            string line = grades.ReadLine();
+            Console.WriteLine("Выберите столбец, по которому вы хотите совершить поиск по справочнику: \n \n 0 - ФИО студента \n 1 - Предмет \n 2 - Оценка \n ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Введите слово для поиска по справочнику: ");
+            string word = Console.ReadLine();
+            int i = 0;
+            int a = 0;
+            Console.Clear();
+            Console.WriteLine($"Результаты поиска: \n");
+            string[] start = { "ФИО студента", "Предмет", "Оценка" };
+            string table = start[0];
+
+            Console.WriteLine($"|{$"{"--"}",2}-|-{$"{"------------------------------------"}",35}----|-{$"----------------",15}----|-{$"---------------",15}-|");
+            Console.WriteLine($"|{$"{"№"}",2} | {$"{start[0]}",35}     | {$"{start[1]}",15}     | {$"{start[2]}",15} |");
+            Console.WriteLine($"|{$"{"--"}",2}-|-{$"{"------------------------------------"}",35}----|-{$"----------------",15}----|-{$"---------------",15}-|");
+
+            while (line == word || line != null)
+            {
+                a++;
+                string[] splitLine = line.Split(";");
+                string search = splitLine[b];
+                line = grades.ReadLine();
+                if (word.Contains(search))
+                {
+                    Console.WriteLine($"|{$"{a}",2} | {$"{splitLine[0]}",35}     | {$"{splitLine[1]}",15}     | {$"{splitLine[2]}",15} |");
+                    Console.WriteLine($"|{$"{"--"}",2}-|-{$"{"------------------------------------"}",35}----|-{$"----------------",15}----|-{$"---------------",15}-|");
+                    i = 1;
+                }
+                else
+                {
+
+                }
+            }
+            if (i == 1)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Записи с таким словом нет");
+            }
+            grades.Close();
+        }
+        public static void PlusStudentsGrades(string[] DataPlus)
+        {
+            StreamWriter studentsgrades = new StreamWriter(@"C:\Users\zelen\Desktop\ElectronicGradeBook\ElectronicGradeBook\\studentsgrades.txt", append: true);
+            string line = $"{DataPlus[0]};{DataPlus[1]};{DataPlus[2]};{DataPlus[3]}";
+
+            studentsgrades.WriteLine(line);
+            studentsgrades.Close();
+        }
+    }
+}
---
+++ ElectronicGradeBook/Program.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+
+namespace Electronic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool mini_reset = true;
+            bool reset = true;
+            bool big_reset = true;
+            Trace.Listeners.Add(new TextWriterTraceListener(@"C:\Users\zelen\Desktop\ElectronicGradeBook\ElectronicGradeBook\\out.txt"));
+            Trace.AutoFlush = true;
+            Trace.Indent();
+            string choise;
+            string[] students = { " Введите ФИО студента: ", "\n Введите год зачисления: ", "\n Выберите форму обучения: ", "\n Введите специальность: " };
+            string[] StudentsData = new string[4];
+            string[] grades = { " Введите ФИО студента: ", "\n Введите предмет: ", "\n Введите оценку: " };
+            string[] GradeData = new string[4];
+
+            while (big_reset == true)
+            {
+                Console.Clear();
+                Console.WriteLine("ЭЛЕКТРОННЫЙ ЖУРНАЛ \n");
+                Console.WriteLine("Выберите справочник, с которым вы будете работать:\n \n 1 - Студенты \n 2 - Журнал успеваемости \n 3 - Выйти из программы \n");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        reset = true;
+
+                        Console.Clear();
+                        Console.WriteLine("Справочник: Студенты");
+                        Console.WriteLine();
+                        Console.WriteLine("Выберите действие: \n \n 1 - Добавить запись \n 2 - Просмотреть записи \n 3 - Поиск по справочнику \n");
+                        Console.WriteLine("Enter --> Назад \n");
+
+                        switch (Console.ReadLine())
+                        {
+                            case "1":
+                                Console.Clear();
+                                while (reset == true)
+                                {
+                                    for (int i = 0; i < students.Length; i++)
+                                    {
+                                        Console.WriteLine(students[i]);
+                                        StudentsData[i] = Console.ReadLine();
+                                    }
+
+                                    Base.PlusStudents(StudentsData);
+                                    Base.PlusStudentsGrades(StudentsData);
+                                    Debug.WriteLine("\n Запсись добавлена.\n");
+                                    Trace.WriteLine("\n Запсись добавлена.\n");
+
+                                    mini_reset = true;
+                                    while (mini_reset == true)
+                                    {
+                                        Console.WriteLine("\n Хотите добавить еще одну запись? да/нет \n");
+                                        choise = Console.ReadLine();
+                                        Console.WriteLine();
+
+                                        if (choise == "да")
+                                        {
+                                            reset = true;
+                                            mini_reset = false;
+                                            Console.Clear();
+                                            Debug.WriteLine("\n Пользователь решил добавить еще одну запись.\n");
+                                            Trace.WriteLine("\n Пользователь решил добавить еще одну запись.\n");
+                                        }
+                                        else if (choise == "нет")
+                                        {
+                                            reset = false;
+                                            mini_reset = false;
+                                            Debug.WriteLine("\n Пользователь решил не добавлять запись.\n");
+                                            Trace.WriteLine("\n Пользователь решил не добавлять запись.\n");
+                                        }
+                                        else
+                                        {
+                                            Console.Clear();
+                                            Console.ForegroundColor = ConsoleColor.Red;
+                                            Console.WriteLine("\n Вы ввели неверную команду!!! \n");
+                                            Console.ForegroundColor = ConsoleColor.White;
+                                            reset = false;
+                                            Debug.WriteLine("\n Пользователь ввел неверную команду.\n");
+                                            Trace.WriteLine("\n Пользователь ввел неверную команду.\n");
+                                        }
+                                    }
+                                }
+                                break;
+                            case "2":
+                                try
+                                {
+                                    Console.Clear();
+                                    reset = true;
+                                    Console.WriteLine("\n Записи справочника Студенты \n");
+                                    Base.ReadStudents();
+                                    Debug.WriteLine("\n Пользователь открыл записи справочника Студенты.\n");
+                                    Trace.WriteLine("\n Пользователь открыл записи справочника Студенты.\n");
+                                    Console.WriteLine("\n 1 - выход в главное меню \n");
+
+                                    while (reset == true)
+                                    {
+                                        choise = Console.ReadLine();
+                                        if (choise == "1")
+                                        {
+                                            reset = false;
+                                        }
+                                        else
+                                        {
+                                            Console.Clear();
+                                            Console.ForegroundColor = ConsoleColor.Red;
+                                            Console.WriteLine("\n Вы ввели неверную команду!!! \n");
+                                            Console.ForegroundColor = ConsoleColor.White;
+                                            Debug.WriteLine("\n Пользователь ввел неверную команду.\n");
+                                            Trace.WriteLine("\n Пользователь ввел неверную команду.\n");
+                                            Console.WriteLine("1 - выход в главное меню \n");
+                                        }
+                                    }
+                                }
+                                catch (Exception)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("\n Ошибка!!! Нет записей!!! \n");
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    Console.ReadLine();
+                                }
+                                break;
+                            case "3":
+                                Base.SearchStudents();
+                                Console.WriteLine("\n 1 - выход в главное меню \n");
+
+                                while (reset == true)
+                                {
+                                    choise = Console.ReadLine();
+                                    if (choise == "1")
+                                    {
+                                        reset = false;
+                                    }
+                                    else
+                                    {
+                                        Console.Clear();
+                                        Console.ForegroundColor = ConsoleColor.Red;
+                                        Console.WriteLine("\n Вы ввели неверную команду!!! \n");
+                                        Console.ForegroundColor = ConsoleColor.White;
+                                        Debug.WriteLine("\n Пользователь ввел неверную команду.\n");
+                                        Trace.WriteLine("\n Пользователь ввел неверную команду.\n");
+                                        Console.WriteLine("\n 1 - выход в главное меню \n");
+                                    }
+                                }
+                                break;
+                        }
+                        break;
+                    case "2":
+                        reset = true;
+                        Console.Clear();
+                        Console.WriteLine("Справочник: Журнал успеваемости \n");
+                        Console.WriteLine("Выберите действие: \n \n 1 - Добавить запись \n 2 - Просмтреть записи \n 3 - Поиск по справочнику \n");
+                        Console.WriteLine("Enter --> Назад \n");
+
+                        switch (Console.ReadLine())
+                        {
+                            case "1":
+                                Console.Clear();
+
+                                while (reset == true)
+                                {
+                                    int a = 0;
+                                    for (int i = 0; i < grades.Length; i++)
+                                    {
+                                        Console.WriteLine(grades[i]);
+                                        GradeData[i] = Console.ReadLine();
+
+                                        StreamReader studentslist = new StreamReader(@"C:\Users\zelen\Desktop\ElectronicGradeBook\ElectronicGradeBook\\studentsgrades.txt");
+                                        string line = studentslist.ReadLine();
+
+                                        if (i == 0)
+                                        {
+                                            while (line != null)
+                                            {
+                                                string[] splitLine = line.Split(";");
+                                                string search = splitLine[0];
+                                                line = studentslist.ReadLine();
+                                                if (GradeData[i].Contains(search))
+                                                {
+                                                    a = 1;
+                                                }
+                                                else
+                                                {
+
+                                                }
+                                            }
+                                        }
+                                        else 
+                                        {
+                                            
+                                        }
+                                    }
+
+                                    if (a == 1)
+                                    {
+                                        Base.PlusGrade(GradeData);
+                                    }
+                                    else
+                                    {
+                                        Console.ForegroundColor = ConsoleColor.Red;
+                                        Console.WriteLine(" \n Такого студента не существует \n ");
+                                        Console.ForegroundColor = ConsoleColor.White;
+                                    }
+                                    mini_reset = true;
+
+                                    while (mini_reset == true)
+                                    {
+                                        Console.WriteLine("\n Хотите добавить еще одну запись? да/нет \n");
+                                        choise = Console.ReadLine();
+                                        Console.WriteLine();
+
+                                        if (choise == "да")
+                                        {
+                                            reset = true;
+                                            mini_reset = false;
+                                            Debug.WriteLine("\n Пользователь решил добавить еще одну запись.\n");
+                                            Trace.WriteLine("\n Пользователь решил добавить еще одну запись.\n");
+                                            Console.Clear();
+
+                                        }
+                                        else if (choise == "нет")
+                                        {
+                                            reset = false;
+                                            mini_reset = false;
+                                            Debug.WriteLine("\n Пользователь решил не добавлять еще одну запись.\n");
+                                            Trace.WriteLine("\n Пользователь решил не добавлять еще одну запись.\n");
+                                        }
+                                        else
+                                        {
+                                            Console.Clear();
+                                            Console.ForegroundColor = ConsoleColor.Red;
+                                            Console.WriteLine("\n Вы ввели неверную команду!!! \n");
+                                            Console.ForegroundColor = ConsoleColor.White;
+                                            Debug.WriteLine("\n Пользователь ввел неверную команду.\n");
+                                            Trace.WriteLine("\n Пользователь ввел неверную команду.\n");
+                                            reset = false;
+                                        }
+                                    }
+                                }
+                                break;
+                            case "2":
+                                try
+                                {
+                                    reset = true;
+                                    Console.Clear();
+                                    Console.WriteLine("\n Записи справочника Журнал успеваемости \n");
+                                    Debug.WriteLine("\n Пользователь открыл записи справочника Журнал успеваемости.\n");
+                                    Trace.WriteLine("\n Пользователь открыл записи справочника Журнал успеваемости.\n");
+                                    Base.ReadGrade();
+                                    Console.WriteLine("\n 1 - выход в главное меню \n");
+
+                                    while (reset == true)
+                                    {
+                                        choise = Console.ReadLine();
+
+                                        if (choise == "1")
+                                        {
+                                            reset = false;
+                                        }
+                                        else
+                                        {
+                                            Console.Clear();
+                                            Console.ForegroundColor = ConsoleColor.Red;
+                                            Console.WriteLine("\n Вы ввели неверную команду!!! \n");
+                                            Console.ForegroundColor = ConsoleColor.White;
+                                            Debug.WriteLine("\n Пользователь ввел неверную команду.\n");
+                                            Trace.WriteLine("\n Пользователь ввел неверную команду.\n");
+                                            Console.WriteLine("1 - выход в главное меню \n");
+                                        }
+                                    }
+                                }
+                                catch (Exception)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("\n Ошибка!!! Нет записей!!! \n");
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    Console.ReadLine();
+                                }
+                                break;
+                            case "3":
+                                Base.SearchGrades();
+                                Console.WriteLine("\n 1 - выход в главное меню \n");
+
+                                while (reset == true)
+                                {
+                                    choise = Console.ReadLine();
+                                    if (choise == "1")
+                                    {
+                                        reset = false;
+                                    }
+                                    else
+                                    {
+                                        Console.Clear();
+                                        Console.ForegroundColor = ConsoleColor.Red;
+                                        Console.WriteLine("\n Вы ввели неверную команду!!! \n");
+                                        Console.ForegroundColor = ConsoleColor.White;
+                                        Debug.WriteLine("\n Пользователь ввел неверную команду.\n");
+                                        Trace.WriteLine("\n Пользователь ввел неверную команду.\n");
+                                        Console.WriteLine("\n 1 - выход в главное меню \n");
+                                    }
+                                }
+                                break;
+                        }
+                        break;
+                    case "3":
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+            Trace.Unindent();
+            Trace.Flush();
+            Console.ReadKey();
+        }
+    }
+}
